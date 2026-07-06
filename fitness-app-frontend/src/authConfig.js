fb6fde3c8b26c2460eee9d6e01a63d9e90187933
Priyanshu0486 <132246721+Priyanshu0486@users.noreply.github.com>
2026-07-07 00:15:26 +0530

    Add Caddy HTTPS reverse proxy with DuckDNS domain

--- fitness-app-frontend/src/authConfig.js
+++ fitness-app-frontend/src/authConfig.js
@@ -1,10 +1,10 @@
 export const authConfig = {
   clientId: 'oauth2-pkce-client',
-  authorizationEndpoint: 'http://34.66.134.253:8181/realms/fitness-oauth2/protocol/openid-connect/auth',
-  tokenEndpoint: 'http://34.66.134.253:8181/realms/fitness-oauth2/protocol/openid-connect/token',
-  logoutEndpoint: 'http://34.66.134.253:8181/realms/fitness-oauth2/protocol/openid-connect/logout',
-  redirectUri: 'http://34.66.134.253:5173/',
-  postLogoutRedirectUri: 'http://34.66.134.253:5173/',
+  authorizationEndpoint: 'https://priyanshu-fitness.duckdns.org/realms/fitness-oauth2/protocol/openid-connect/auth',
+  tokenEndpoint: 'https://priyanshu-fitness.duckdns.org/realms/fitness-oauth2/protocol/openid-connect/token',
+  logoutEndpoint: 'https://priyanshu-fitness.duckdns.org/realms/fitness-oauth2/protocol/openid-connect/logout',
+  redirectUri: 'https://priyanshu-fitness.duckdns.org/',
+  postLogoutRedirectUri: 'https://priyanshu-fitness.duckdns.org/',
   scope: 'openid profile email offline_access',
   onRefreshTokenExpire: (event) => event.logIn(),
 }
