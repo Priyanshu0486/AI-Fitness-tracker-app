e26f331654ececf8265a4bc8f4993849cf56456c
Priyanshu0486 <132246721+Priyanshu0486@users.noreply.github.com>
2026-07-07 00:01:19 +0530

    Update frontend URLs to point to external IP

--- fitness-app-frontend/src/authConfig.js
+++ fitness-app-frontend/src/authConfig.js
@@ -1,10 +1,10 @@
 export const authConfig = {
   clientId: 'oauth2-pkce-client',
-  authorizationEndpoint: 'http://localhost:8181/realms/fitness-oauth2/protocol/openid-connect/auth',
-  tokenEndpoint: 'http://localhost:8181/realms/fitness-oauth2/protocol/openid-connect/token',
-  logoutEndpoint: 'http://localhost:8181/realms/fitness-oauth2/protocol/openid-connect/logout',
-  redirectUri: 'http://localhost:5173/',
-  postLogoutRedirectUri: 'http://localhost:5173/',
+  authorizationEndpoint: 'http://34.66.134.253:8181/realms/fitness-oauth2/protocol/openid-connect/auth',
+  tokenEndpoint: 'http://34.66.134.253:8181/realms/fitness-oauth2/protocol/openid-connect/token',
+  logoutEndpoint: 'http://34.66.134.253:8181/realms/fitness-oauth2/protocol/openid-connect/logout',
+  redirectUri: 'http://34.66.134.253:5173/',
+  postLogoutRedirectUri: 'http://34.66.134.253:5173/',
   scope: 'openid profile email offline_access',
   onRefreshTokenExpire: (event) => event.logIn(),
 }
